a1b7949c99a28218dab38825fe8240f6369df13a
Patrick Feiring <pfeiring@live.com>
2023-04-06 15:44:31 +0200

    style: format skeletons

--- nvim/lua/user/snippets/skeletons.lua
+++ nvim/lua/user/snippets/skeletons.lua
@@ -15,10 +15,8 @@ local i = luasnip.insert_node
 local t = luasnip.text_node
 local s = luasnip.snippet
 
-local skeleton_group = vim.api.nvim_create_augroup(
-    "skeletons",
-    { clear = true }
-)
+local skeleton_group =
+    vim.api.nvim_create_augroup("skeletons", { clear = true })
 
 local function read_skeleton_file(name)
     local path = vim.fn.stdpath("config") .. "/templates/" .. name
@@ -62,10 +60,8 @@ vim.api.nvim_create_autocmd({ "BufNewFile" }, {
         end
 
         local current_basename = vim.fn.expand("%:t")
-        local component_name = string.match(
-            current_basename,
-            "([^%.]*)%.stories%.ts"
-        )
+        local component_name =
+            string.match(current_basename, "([^%.]*)%.stories%.ts")
 
         if not component_name then
             return
