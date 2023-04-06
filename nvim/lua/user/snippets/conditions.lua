4e8b3d22b3a6734fb0baaeac7b92f2cfc1172480
Patrick Feiring <pfeiring@live.com>
2023-04-06 15:28:54 +0200

    feat: use native luasnip condition objects

--- nvim/lua/user/snippets/conditions.lua
+++ nvim/lua/user/snippets/conditions.lua
@@ -1,27 +1,13 @@
 local M = {}
 
-function M.and_(first, second)
+local conditions = require("luasnip.extras.conditions")
+
+M.filetype_is = function(filetype)
     local function condition(line_to_cursor, matched_trigger)
-        if first(line_to_cursor, matched_trigger) then
-            return second(line_to_cursor, matched_trigger)
-        else
-            return false
-        end
+        return vim.bo.filetype == filetype
     end
 
-    return condition
-end
-
-function M.or_(first, second)
-    local function condition(line_to_cursor, matched_trigger)
-        if first(line_to_cursor, matched_trigger) then
-            return true
-        else
-            return second(line_to_cursor, matched_trigger)
-        end
-    end
-
-    return condition
+    return conditions.make_condition(condition)
 end
 
 return M
