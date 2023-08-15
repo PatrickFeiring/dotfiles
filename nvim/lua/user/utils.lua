bf46a5179c5ec6faaaa3e4725c1f6163477f2f2a
Patrick Feiring <pfeiring@live.com>
2023-08-15 07:53:55 +0200

    fix: ensure correct sort order

--- nvim/lua/user/utils.lua
+++ nvim/lua/user/utils.lua
@@ -98,6 +98,16 @@ local function are_paths_in_routes(a, b)
         and string.find(b.path, "src/routes/")
 end
 
+local function get_files_first_score(a, b)
+    if a.type == b.type then
+        return 0
+    elseif a.type < b.type then
+        return -1
+    else
+        return 1
+    end
+end
+
 local filename_order = {
     "+server.ts",
     "+layout.server.ts",
@@ -123,41 +133,31 @@ local function get_sort_order_by_filename(a)
         end
     end
 
-    return 0
-end
-
-local function sort_directories_first(a, b)
-    if a.type == b.type then
-        return a.path < b.path
-    else
-        return a.type > b.type
-    end
-end
-
-local function sort_files_first(a, b)
-    if a.type == b.type then
-        return a.path < b.path
-    else
-        return a.type < b.type
-    end
+    return #filename_order + 1
 end
 
 ---Sort project paths
 function M.sort_project_paths(a, b)
-    local special_a = get_sort_order_by_filename(a)
-    local special_b = get_sort_order_by_filename(b)
-
-    if special_a ~= special_b then
-        return special_a < special_b
-    end
-
     -- Sort by files first in Svelte routes folder
     -- https://www.reddit.com/r/sveltejs/comments/xltgyp/quality_of_life_tips_when_using_sveltekit_in_vs/
+    local score_difference = get_files_first_score(a, b)
+
     if are_paths_in_routes(a, b) then
-        return sort_directories_first(a, b)
-    else
-        return sort_files_first(a, b)
+        score_difference = score_difference * -1
     end
+
+    if score_difference ~= 0 then
+        return score_difference < 0
+    end
+
+    local filename_score_difference = get_sort_order_by_filename(a)
+        - get_sort_order_by_filename(b)
+
+    if filename_score_difference ~= 0 then
+        return filename_score_difference < 0
+    end
+
+    return a.path < b.path
 end
 
 return M
