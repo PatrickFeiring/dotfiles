1ee59541a471a6ca3bbf8d49d135e99cf96acc4d
Patrick Feiring <pfeiring@live.com>
2024-05-19 19:53:25 +0200

    feat: add inlay hint toggle

--- nvim/lua/user/lsps.lua
+++ nvim/lua/user/lsps.lua
@@ -12,6 +12,9 @@ vim.diagnostic.config({
 
 vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
 vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
+vim.keymap.set("n", "<space>i", function()
+    vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+end)
 
 local function get_diagnostic_under_cursor()
     local line, row = unpack(vim.api.nvim_win_get_cursor(0))
