7293a2bdb5c69b520b95c5e0bb55d1e89e8ae982
Patrick Feiring <pfeiring@live.com>
2025-04-26 12:21:38 +0200

    chore: loop is now renamed to uv

--- nvim/lua/user/init.lua
+++ nvim/lua/user/init.lua
@@ -8,7 +8,7 @@ require("user.general")
 -- Bootstrap lazy
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
-if not vim.loop.fs_stat(lazypath) then
+if not vim.uv.fs_stat(lazypath) then
     vim.fn.system({
         "git",
         "clone",
