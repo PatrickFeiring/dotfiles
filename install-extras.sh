82a4e3018c80a69d96a04b5beb472890f5688507
Patrick Feiring <pfeiring@live.com>
2025-04-27 13:12:52 +0200

    chore: add script installing additional packages

---
+++ install-extras.sh
@@ -0,0 +1,25 @@
+
+brew install neovim
+brew install ghostty
+brew install pure
+
+brew install fd
+brew install neovim
+brew install ripgrep
+brew install tmux
+brew install tree
+brew install yqrashawn/goku/goku
+brew install fnm
+brew install libpq
+brew install gh
+
+brew install lua-language-server
+brew install stylua
+brew install taplo
+
+cargo install proximity-sort
+cargo install viu
+
+brew install luarocks
+luarocks install busted
+luarocks install luacheck
