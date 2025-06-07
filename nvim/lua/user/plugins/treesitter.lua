da3717aaa86850b759628ef2974b7fe6ceb0d080
Patrick Feiring <pfeiring@live.com>
2025-06-07 12:59:50 +0200

    perf: install treesitter parsers on demand

--- nvim/lua/user/plugins/treesitter.lua
+++ nvim/lua/user/plugins/treesitter.lua
@@ -4,7 +4,10 @@ return {
         build = ":TSUpdate",
         config = function()
             require("nvim-treesitter.configs").setup({
-                ensure_installed = "all",
+                ensure_installed = {},
+                sync_install = false,
+                auto_install = true,
+                ignore_install = {},
 
                 highlight = {
                     enable = true,
